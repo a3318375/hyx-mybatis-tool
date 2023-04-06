0b36cb38b807f50a4b51c46aee2ea32b218b9503
GitHub Action <action@github.com>
2023-04-06 03:54:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/a3318375/hyxmybatistool/MyBundle.kt
+++ src/main/kotlin/com/github/a3318375/hyxmybatistool/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.a3318375.hyxmybatistool
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/a3318375/hyxmybatistool/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/a3318375/hyxmybatistool/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.a3318375.hyxmybatistool.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
